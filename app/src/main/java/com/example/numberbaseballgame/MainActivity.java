9c7c75a04070f2edd79444d5228033cc5c36ae66
bky373 <bo373@naver.com>
2019-12-05 07:37:22 +0900

    done #7-2

--- app/src/main/java/com/example/numberbaseballgame/MainActivity.java
+++ app/src/main/java/com/example/numberbaseballgame/MainActivity.java
@@ -4,14 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.numberbaseballgame.R;
 
+import java.nio.charset.MalformedInputException;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     int[] comNumbers = new int[3];
+    int inputTextCount = 0;
+
+    TextView[] inputTextView = new TextView[3];
+    Button[] numButton = new Button[10];
 
 
     @Override
@@ -19,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         comNumbers = getComNumbers();
+
+        for (int i = 0; i < inputTextView.length; i++) {
+            inputTextView[i] = findViewById(R.id.input_text_view_0 + i);
+        }
+
+        for (int i = 0; i < numButton.length; i++) {
+            numButton[i] = findViewById(R.id.num_button_0 + i);
+        }
+
+        for(Button getNumButton : numButton)
+            getNumButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (inputTextCount < 3) {
+                        Button button = findViewById(v.getId());
+                        inputTextView[inputTextCount].setText(button.getText().toString());
+                        button.setEnabled(false);
+                        inputTextCount++;
+                    } else {
+                        Toast.makeText(getApplicationContext(), "히트 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
     }
 
     public int[] getComNumbers(){
