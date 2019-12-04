66c7e7eee45bb9dae4269bb58753e931ea32e818
bky373 <bo373@naver.com>
2019-12-04 13:24:18 +0900

    done #5

--- app/src/main/java/com/example/numberbaseballgame/LoopBasic.java
+++ app/src/main/java/com/example/numberbaseballgame/LoopBasic.java
@@ -2,6 +2,8 @@ package com.example.numberbaseballgame;
 
 public class LoopBasic {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
     }
 }
--- app/src/main/java/com/example/numberbaseballgame/MainActivity.java
+++ app/src/main/java/com/example/numberbaseballgame/MainActivity.java
@@ -3,14 +3,37 @@ package com.example.numberbaseballgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.numberbaseballgame.R;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    int[] comNumbers = new int[3];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        comNumbers = getComNumbers();
+    }
+
+    public int[] getComNumbers(){
+        int[] setComNumbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            setComNumbers[i] = new Random().nextInt(10);
+            for (int j = 0; j < i; j++) {
+                if(setComNumbers[i] == setComNumbers[j]){
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        Log.e("setComNumbers","setComNumbers= " + setComNumbers[0] + " " + setComNumbers[1] + " " + setComNumbers[2]);
+        return setComNumbers;
     }
 }
